0acb2d670c2d2aa247e9dd1c306a59efd820643f
TakumiNakamura <155757338+TAKUMI120108@users.noreply.github.com>
2024-02-18 16:04:23 +0000

    [Remove]-bookers

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,27 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+def create
+      book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
+end
+
+  def index
+    　@book = Book.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
+
+private
+def book_params
+  params.require(:book).permit(:title, :body)
+  
+end
---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,28 @@
+class ListsController < ApplicationController
+def new
+  @book = Book.new
+end
+
+def create
+      book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
+end
+
+  def index
+
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
+
+private
+def book_params
+  params.require(:book).permit(:title, :body)
+  
+end
+
