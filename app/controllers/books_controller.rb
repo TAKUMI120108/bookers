44beb002c5befa9b0a9a438c13836b360d277110
TakumiNakamura <155757338+TAKUMI120108@users.noreply.github.com>
2024-02-19 16:54:35 +0000

    [Remove]-book-index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,27 +1,33 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
 def create
       book = Book.new(book_params)
     book.save
-    redirect_to '/books'
+    redirect_to book_path(book.id)
+end
+
+
+def destroy
+  @books = Book.find(params[:id])
 end
 
   def index
-    　@book = Book.all
+     @book = Book.new
+     @books = Book.all
   end
 
   def show
+  @books = Book.find(params[:id])
+  
   end
 
   def edit
+   @books = Book.find(params[:id])
   end
-end
-
+  
 private
 def book_params
   params.require(:book).permit(:title, :body)
   
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   get '/top' => 'homes#top'
   
-  get '/books' => 'books#top'
-  get 'lists/new'
-  get 'books' =>'books#index'
-  get 'lists/show'
-  get 'lists/edit'
+  get '/books/new' 
+  get '/books' =>'books#index'
+  get '/books/:id' => 'books#show', as: 'book'
+  get '/books/:id/edit' => 'books#edit', as: 'edit_book'
   post 'books' => 'books#create' 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
