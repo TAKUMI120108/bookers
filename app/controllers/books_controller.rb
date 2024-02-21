340c4f64c854b425e13f7893b99873aefa7949f0
TakumiNakamura <155757338+TAKUMI120108@users.noreply.github.com>
2024-02-21 16:31:53 +0000

    [remove]booker-layouts

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,21 +9,31 @@ end
 
 def destroy
   @books = Book.find(params[:id])
+  @books.destroy
+  redirect_to '/books'
 end
 
   def index
+
      @book = Book.new
      @books = Book.all
   end
 
   def show
   @books = Book.find(params[:id])
-  
+
   end
 
   def edit
    @books = Book.find(params[:id])
   end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
   
 private
 def book_params
